4c5bbda8b753078c525c7be0f2d0179a02625bf4
Fredy Lievano <fredylg@hotmail.com>
2017-07-01 23:18:28 +1000

    there was a bug

--- app/Http/Controllers/LightsController.php
+++ app/Http/Controllers/LightsController.php
@@ -19,22 +19,22 @@ class LightsController extends Controller
 
     public function getLightStatus(){
        // this kill the microcontroller
-        // $status = $this->getLightStatusSonOff();
-        // if( $status !=  $this->light['status'] ){
-        //     $this->setLightStatusDB( $status );
-        // }
-        // return $status;
+        $status = $this->getLightStatusSonOff();
+        if( $status !=  $this->light['status'] ){
+            $this->setLightStatusDB( $status );
+        }
+        return $status;
         //dd($this->light['status']);
 
-        $value_cached = Cache::get($this->lamp_id.'_value');
-        if ($value_cached == null) {
-          $expiresAt = Carbon::now()->addSeconds(10);
-          Cache::put( $this->lamp_id.'_value', $this->light['status'] , $expiresAt);
-          $status = $this->light['status'];
-        }else{
-          $status = $value_cached;
-        }
-        return $status ;
+        // $value_cached = Cache::get($this->lamp_id.'_value');
+        // if ($value_cached == null) {
+        //   $expiresAt = Carbon::now()->addSeconds(10);
+        //   Cache::put( $this->lamp_id.'_value', $this->light['status'] , $expiresAt);
+        //   $status = $this->light['status'];
+        // }else{
+        //   $status = $value_cached;
+        // }
+        // return $status ;
     }
 
     public function setLightStatus( $status ){
@@ -66,7 +66,7 @@ class LightsController extends Controller
 
     private function setLightStatusSonOff( $status ){
       $on = substr(trim(file_get_contents('http://'.$this->light['ip'].'/cm?cmnd=Power%20' . $status )),-2);
-      if( $on == 'on'){
+      if( $on == 'ON'){
         return 1;
       }else{
         return 0;
